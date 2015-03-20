69d030082bbe68c6f5304da31197e40d9848d48d
Emilien Macchi <emilien@redhat.com>
2015-03-19 22:32:39 -0400

    upgrade: fix nova migration

--- upgrade/files/migrate.sh
+++ upgrade/files/migrate.sh
@@ -21,6 +21,10 @@ set -e
 set -x
 
 COMPUTE=$(cat /etc/hostname)
+export OS_USERNAME=$1
+export OS_TENANT_NAME=$2
+export OS_PASSWORD=$4
+export OS_AUTH_URL=$5
 EXTRA_MIGRATE=
 
 # number of time we loop to check if there are still servers
